8a8ddea830818f2a68ebce19bc101cc310410b4d
liujiujiang <1460083332@qq.com>
2024-03-29 17:46:10 +0800

    完成基本功能

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import './style.css'
-
+// @ts-ignore
+import DrawCallInspector from './DrawCallInspector.js'
+// @ts-ignore
+window.DrawCallInspector = DrawCallInspector;window.THREE = THREE
 async function init() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,16 +17,24 @@ async function init() {
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+  const cube2 = new THREE.Mesh(geometry, material);
+  cube2.position.x = 2
+  scene.add(cube2);
 
+  const dci = new DrawCallInspector(renderer, scene, camera)
+  dci.mount()
+  const controls = new OrbitControls(camera, renderer.domElement);
   camera.position.z = 5;
 
   function animate() {
     requestAnimationFrame(animate);
-
+    controls.update()
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
+    dci.update()
+    dci.begin()
     renderer.render(scene, camera);
+    dci.end()
   }
 
   animate();
